5bc66d3d9f4dba47f59550d32084c47b1b2ae7be
Pravinkumar Singh <pravinsinghkumar@gmail.com>
2019-12-31 19:29:38 +0530

    Create response_of_intent.py

---
+++ response_of_intent.py
@@ -0,0 +1,6 @@
+def sorry_intent_response():
+    return random.choice(sorry_response)+" "+random.choice(sorry_predicate)
+
+
+def goodbye_intent_response():
+    return random.choice(goodbye_response)+" "+random.choice(goodbye_predicate)
