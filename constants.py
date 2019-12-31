76cc240480174cf79f826a585282bf610b2dc411
Pravinkumar Singh <pravinsinghkumar@gmail.com>
2019-12-31 19:28:42 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,13 @@
+
+greetings_key_word=["hi","hello","morning"]
+greeting_response=["Hello There!","Hi!","Aloha!"]
+greeting_predicate=[" I am HelloESG"," I am here to help you"," You can ask me about ESG Ratings, IVA Ratings and other ESG details"]
+
+goodbye_key_word=["bye","thank","good night"]
+goodbye_response=["Thank you for your time","Happy to help",""]
+goodbye_predicate=["See you again","Namastey!","Bye!"]
+
+
+
+sorry_response=["Sorry! Unable to process request","Please rephrase." ]
+sorry_predicate=["You can try finding out esg ratings"," Please try again." ,"I do not have information about this particular query"]
